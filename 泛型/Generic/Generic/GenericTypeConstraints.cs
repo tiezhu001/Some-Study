602f70e7218edf70326566ab2c7640f29d07c4ef
tiezhu001 <szh19961117@163.com>
2020-04-13 13:45:07 +0800

    更新泛型

---
+++ 泛型/Generic/Generic/GenericTypeConstraints.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class GenericTypeConstraints
+    {
+
+    }
+
+    // 1.引用类型约束
+    public class classSample<T> where T : class
+    {
+
+    }
+
+    // 2.值类型约束
+    public class structSample<T> where T : struct
+    {
+
+    }
+
+}
